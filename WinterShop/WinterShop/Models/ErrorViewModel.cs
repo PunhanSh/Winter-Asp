7ab17737d38ede639c1ab61b12598b35ccc9eaa8
PunhanSh <punhanash@code.edu.az>
2021-12-07 13:37:23 +0400

    Added Task

---
+++ WinterShop/WinterShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WinterShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
